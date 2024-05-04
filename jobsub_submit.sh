06dd76eb5cb036a16197f88b5b3ec4f9e6d2da25
Alex Wilkinson <alexander.wilkinson.20@ucl.ac.uk>
2024-05-04 14:19:46 -0500

    typo fix

--- jobsub_submit.sh
+++ jobsub_submit.sh
@@ -5,9 +5,7 @@ OUTPUT_ND_CAF_DIR=$2
 
 echo "Running on $(hostname) at ${GLIDEIN_Site}. GLIDEIN_DUNESite = ${GLIDEIN_DUNESite}"
 
-cp ${INPUT_TAR_DIR_LOCAL}/* .
-
-ls -lrth
+cp -r ${INPUT_TAR_DIR_LOCAL}/* .
 
 source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh
 setup root v6_28_12 -q e20:p3915:prof
@@ -20,8 +18,10 @@ input_file=$(ifdh ls $INPUT_ND_CAF_DIR | head -n $((PROCESS+2)) | tail -n -1)
 input_name=${input_file##*/}
 input_name=${input_name%.*}
 input_name="${input_name}_fdrecpred.root"
-ifdh cp -D $input_file $input_name
+ifdh cp $input_file $input_name
 
-python nd_fd_translate_caf.py $input_name model_weights/model.pt
+ls -lrth
+
+python ndfd_translate_caf.py $input_name model_weights/model.pt
 
 ifdh cp $input_name ${OUTPUT_ND_CAF_DIR}/$input_name
