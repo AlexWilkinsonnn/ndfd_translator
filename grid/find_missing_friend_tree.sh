0c724794a92a0cba5b28d9318e249ae2f9b7a1be
Alex Wilkinson <alexander.wilkinson.20@ucl.ac.uk>
2024-07-19 12:11:17 -0500

    check number of friend tree entries is correct

--- grid/find_missing_friend_tree.sh
+++ grid/find_missing_friend_tree.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # Check for presence of FDRecoNumuPredFriend tree in the the output root files
+# Check that FDRecoNumuPredFriend has the same number of entries as cafTree
 # Requires duneutil and root
 
-DIR="/pnfs/dune/persistent/users/awilkins/ND_CAF_fdrecopreds/chasnip-NDCAF_OnAxisHadd-FHC"
-OUT_FILE="/exp/dune/app/users/awilkins/ndfd_translator/missing_friendtree.txt"
+DIR="/pnfs/dune/persistent/users/awilkins/ND_CAF_fdrecopreds/chasnip-NDCAF_OnAxisHadd-FHC_fdrecopreds"
+MISSING_OUT_FILE="/exp/dune/app/users/awilkins/ndfd_translator/grid/missing_friendtree.txt"
+INCOMPLETE_OUT_FILE="/exp/dune/app/users/awilkins/ndfd_translator/grid/incomplete_friendtree.txt"
 
 cd $DIR
 
@@ -21,7 +23,20 @@ do
   )
   if [[ $has_friend == 0 ]]
   then
-    echo $file >> $OUT_FILE
+    echo $file >> $MISSING_OUT_FILE
+  else
+    complete_friend=$( \
+      root -b \
+           -l \
+           -q \
+           `pnfs2xrootd $file` \
+           -e 'std::cout << "Result " << (int)(cafTree->GetEntries() == FDRecoNumuPredFriend->GetEntries()) << std::endl;' 2>/dev/null \
+           | grep "Result " \
+           | sed "s/Result //" \
+    )
+    if [[ $complete_friend == 0 ]]
+    then
+      echo $file >> $INCOMPLETE_OUT_FILE
+    fi
   fi
 done
-
