2da55d7474433b84e2238c8d50e7fe8ddf90b4f0
Alex Wilkinson <alexander.wilkinson.20@ucl.ac.uk>
2024-11-01 13:54:44 -0500

    update jobsub script

--- grid/jobsub_submit.sh
+++ grid/jobsub_submit.sh
@@ -7,6 +7,7 @@ echo "Running on $(hostname) at ${GLIDEIN_Site}. GLIDEIN_DUNESite = ${GLIDEIN_DU
 
 cp -r ${INPUT_TAR_DIR_LOCAL}/* .
 
+# Setup env
 source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh
 setup root v6_28_12 -q e20:p3915:prof
 setup ifdhc
@@ -14,6 +15,7 @@ python -m venv .venv_3_9_15_torch
 source .venv_3_9_15_torch/bin/activate
 pip install torch==2.0
 
+# Find and copy over input file
 input_file=$(ifdh ls $INPUT_ND_CAF_DIR | head -n $((PROCESS+2)) | tail -n -1)
 input_name=${input_file##*/}
 input_name=${input_name%.*}
@@ -23,10 +25,13 @@ ifdh cp $input_file $input_name
 
 ls -lrth
 
+# Do the translation
 python ndfd_translate_caf.py --vertices_file bin/train_vertices.npy \
+                             --model_config bin/models/config_fhc_numu-numu_muresim.json \
                              $input_name \
-                             bin/model_weights/model_fhc_numu-numu_oldg4params_fixedEhadcut.pt
+                             bin/models/model_fhc_numu-numu_muresim.pt
 
+# Copy file back if translation exitted successfully
 if [[ $? == 0 ]]
 then
   ifdh cp $input_name ${OUTPUT_ND_CAF_DIR}/$input_name
